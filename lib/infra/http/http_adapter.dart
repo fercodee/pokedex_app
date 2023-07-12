d25a27da0b1da7ad2d508b2e34efeab8bd557407
fercodee <devbyfull@gmail.com>
2023-07-12 01:12:57 -0300

    ref: move HttpAdapter to own file

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -1,1 +1,51 @@
-class HttpAdapter {}
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import '../../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  @override
+  Future<Map> request(
+      {required String url, required String method, String? pathParam}) async {
+    final uri = Uri.https('example.com', url);
+    Response response = Response('', 500);
+
+    try {
+      if (method == 'get') {
+        response = await client.get(uri, headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        });
+      }
+    } catch (e) {
+      throw HttpError.serverError;
+    }
+
+    return _handleResponse(response);
+  }
+
+  Map _handleResponse(Response response) {
+    if (response.statusCode == 200) {
+      return response.body.isEmpty
+          ? jsonDecode('{}')
+          : jsonDecode(response.body);
+    } else if (response.statusCode == 204) {
+      return jsonDecode('{}');
+    } else if (response.statusCode == 400) {
+      throw HttpError.badRequest;
+    } else if (response.statusCode == 401) {
+      throw HttpError.unauthorized;
+    } else if (response.statusCode == 403) {
+      throw HttpError.forbidden;
+    } else if (response.statusCode == 404) {
+      throw HttpError.notFound;
+    } else {
+      throw HttpError.serverError;
+    }
+  }
+}
